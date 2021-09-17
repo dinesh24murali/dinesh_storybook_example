0c0ed83e8b2271a6d779a01f22656a44ee47a92f
dinesh24murali <dinesh24murali@gmail.com>
2021-09-17 08:34:50 +0530

    first commit

---
+++ src/stories/Button.stories.jsx
@@ -0,0 +1,20 @@
+// src/components/Button.stories.js
+import React from 'react';
+
+import Button from './Button';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  label: 'My Button',
+};
